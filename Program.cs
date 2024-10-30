694c80144b7fbdbaf741ffa5d4d2b5fd8b461a50
shotelliactions <shotelli+githubactions@nysif.com>
2024-10-30 14:58:43 -0400

    Added comment

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace pipelines_dotnet_core
-{
+{ //change
     public class Program
     {
         public static void Main(string[] args)
